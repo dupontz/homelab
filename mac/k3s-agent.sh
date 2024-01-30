d6c67da224b122b6dffeba9cef140abd97f589f0
dupontz <gustavo.dupont@gmail.com>
2024-01-30 15:39:48 -0800

    setting tailscale

---
+++ mac/k3s-agent.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TOKEN=""
+TAILSCALE_AUTH_TOKEN=tskey-auth-k3i22C5CNTRL-vNKvFZB5nBgVLbZNJgZwBgD2k8a8yAKUe
+
+while [ -z "$TOKEN" ]
+do
+	sleep 1
+	TOKEN=$(echo sudo cat /var/lib/rancher/k3s/server/node-token | ssh -i /home/ubuntu/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@198.19.0.1 | grep '::server:')
+    SERVER_IP=$( ssh -i /home/ubuntu/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@198.19.0.1 ifconfig tailscale0 | awk -F ' *|:' '/inet /{print $3}' | grep .)
+done
+
+echo "Token acquired: $TOKEN"
+
+while [ ! -d /var/lib/rancher ]
+do
+	curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.25.15+k3s2" K3S_URL=https://$SERVER_IP:6443 K3S_TOKEN=$TOKEN INSTALL_K3S_EXEC="--vpn-auth=name=tailscale,joinKey=tailscale,joinKey=$TAILSCALE_AUTH_TOKEN sh -
+	sleep 1
+done
+
+echo Installation Completed.
+
--- mac/k3s-control.sh
+++ mac/k3s-control.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 while [ ! -d /var/lib/rancher ]
-do
-	curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.25.15+k3s2" INSTALL_K3S_EXEC="--flannel-backend=none --cluster-cidr=198.19.16.0/20 --service-cidr=198.19.32.0/20 --write-kubeconfig-mode 664 --disable-network-policy" sh -
+do  
+
+IP=$(ifconfig tailscale0 | awk -F ' *|:' '/inet /{print $3}' )
+TAILSCALE_AUTH_TOKEN=tskey-auth-k3i22C5CNTRL-vNKvFZB5nBgVLbZNJgZwBgD2k8a8yAKUe
+# Install tailscale
+    curl -fsSL https://tailscale.com/install.sh | sh
+
+# Is flannel enough ? no need to add calico later on
+	curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.25.15+k3s2" INSTALL_K3S_EXEC="--vpn-auth=name=tailscale,joinKey=tailscale,joinKey=$TAILSCALE_AUTH_TOKEN --bind-address $IP --write-kubeconfig-mode 664 --disable-network-policy" sh -
+#	curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.25.15+k3s2" INSTALL_K3S_EXEC="--vpn-auth=name=tailscale,joinKey=tailscale,joinKey=tskey-auth-k3i22C5CNTRL-vNKvFZB5nBgVLbZNJgZwBgD2k8a8yAKUe --flannel-backend=none --cluster-cidr=198.19.16.0/20 --service-cidr=198.19.32.0/20 --write-kubeconfig-mode 664 --disable-network-policy" sh -
 	sleep 1
 done
 
