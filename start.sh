8611bea9f518f714ab92ed210a36dd14915a75fb
dupontz <gustavo.dupont@gmail.com>
2024-01-30 20:26:30 -0800

    ps

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #/bin/bash
 
 curl https://raw.githubusercontent.com/dupontz/homelab/main/mac/control-init.yaml | multipass launch -vvvv -n control 20.04 --memory 3G --cloud-init -
+ curl https://raw.githubusercontent.com/dupontz/homelab/main/mac/node1-init.yaml | multipass launch -vvvv -n node1 20.04 --cloud-init - 
+
+
+ #ref https://blog.dsb.dev/posts/accessing-my-k3s-cluster-from-anywhere-with-tailscale/
+ #https://docs.k3s.io/installation/network-options#integration-with-the-tailscale-vpn-provider-experimental
