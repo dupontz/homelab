8bf3b053ca55c7787ff4c85d445235e261e9839d
dupontz <gustavo.dupont@gmail.com>
2024-01-30 19:27:09 -0800

    fix syntax

--- mac/k3s-control.sh
+++ mac/k3s-control.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
-
-
 IP=""
 
 while [ -z "$IP" ]
 do
-	if [ $( ifconfig -s | grep tail ) ]; 
-	then
+	if ifconfig -s | grep tails ; then
 		IP=$( ifconfig tailscale0 | awk -F ' *|:' '/inet /{print $3}' | grep .)
 	fi
 	sleep 2
 done
 
 while [ ! -d /var/lib/rancher ]
-do  
+do
 
 # Is flannel enough ? no need to add calico later on
 	curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.25.15+k3s2" INSTALL_K3S_EXEC="--flannel-iface tailscale0 --advertise-address $IP --node-ip $IP  --node-external-ip $IP --write-kubeconfig-mode 664" sh -
 #	curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.25.15+k3s2" INSTALL_K3S_EXEC="--vpn-auth=name=tailscale,joinKey=tailscale,joinKey=tskey-auth-k3i22C5CNTRL-vNKvFZB5nBgVLbZNJgZwBgD2k8a8yAKUe --flannel-backend=none --cluster-cidr=198.19.16.0/20 --service-cidr=198.19.32.0/20 --write-kubeconfig-mode 664 --disable-network-policy" sh -
 #	curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.25.15+k3s2" INSTALL_K3S_EXEC="--vpn-auth=name=tailscale,joinKey=tailscale,joinKey=$TAILSCALE_AUTH_TOKEN  --write-kubeconfig-mode 664 --disable-network-policy" sh -
-   	
+
 	sleep 1
 done
 
