7b8d6a1da446d1a2e15d74516abe65a18bc3a8b3
dupontz <gustavo.dupont@gmail.com>
2024-01-30 19:31:12 -0800

    waiting for tailscale ip

--- mac/k3s-agent.sh
+++ mac/k3s-agent.sh
@@ -13,9 +13,19 @@ done
 echo "Token acquired: $TOKEN"
 echo "Server ip: $SERVER_IP"
 
+IP=""
+
+while [ -z "$IP" ]
+do
+	if ifconfig -s | grep tails ; then
+		IP=$( ifconfig tailscale0 | awk -F ' *|:' '/inet /{print $3}' | grep .)
+	fi
+	sleep 2
+done
+
 while [ ! -d /var/lib/rancher ]
 do
-	IP=$( ifconfig tailscale0 | awk -F ' *|:' '/inet /{print $3}' | grep .)
+
 
 	curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.25.15+k3s2" K3S_URL=https://$SERVER_IP:6443 K3S_TOKEN=$TOKEN INSTALL_K3S_EXEC="--flannel-iface tailscale0 --node-ip $IP --node-external-ip $IP" sh -
 	sleep 1
